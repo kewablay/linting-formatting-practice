48a2c1c571c051d7ad4238016a471ee0f47eaacb
kewablay <kojokewajunior123@gmail.com>
2024-09-13 11:48:41 +0000

    fixed all eslint warnings in code

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
+import { User } from '../interfaces';
 
 @Component({
   selector: 'app-user-list',
+  standalone: true,
   template: `
     <ul>
-      <li *ngFor="let user of users">{{ user.name }} ({{ user.age }})</li>
+      @for (user of users; track user) {
+        <li>{{ user.name }} ({{ user.age }})</li>
+      }
     </ul>
   `,
-  styles: [`
-    ul { list-style-type: none; padding: 0; }
-    li { margin-bottom: 10px; }
-  `]
+  styles: [
+    `
+      ul {
+        list-style-type: none;
+        padding: 0;
+      }
+      li {
+        margin-bottom: 10px;
+      }
+    `,
+  ],
 })
 export class UserListComponent implements OnInit {
-  users: any[];
+  users: User[] = [];
 
   constructor(private userService: UserService) {}
 
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
+import { User } from './interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-  private users = [{name: 'John', age: 30}, {name: 'Jane', age: 25}];
+  private users = [
+    { name: 'John', age: 30 },
+    { name: 'Jane', age: 25 },
+  ];
 
-  constructor() { }
+  constructor() {}
 
   getUsers() {
     return this.users;
   }
 
-  addUser(user) {
+  addUser(user: User) {
     this.users.push(user);
   }
 }
